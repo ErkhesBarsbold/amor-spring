875acd8138d4112fe18c32e911b66102f8ce37b6
Erkhes <Erkhes@Mimosa.router17d35e.com>
2019-07-29 12:15:41 +0800

    first push

---
+++ src/main/java/mn/amor/amorspring/AmorSpringApplication.java
@@ -0,0 +1,13 @@
+package mn.amor.amorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmorSpringApplication.class, args);
+	}
+
+}
